bbb70a553b6448f799f8bfbd6681744bfcabab33
Nont Chaiyakarn <nontc5@hotmail.com>
2016-10-27 20:50:31 +0700

    New Response message

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello'.$text
+				'text' => 'Bot Response: '.$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
