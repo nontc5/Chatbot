6359f018a9a7248414fe0878f93c1283259e83be
Nont Chaiyakarn <nontc5@hotmail.com>
2016-12-28 23:02:33 +0700

    Update Survey

--- bot.php
+++ bot.php
@@ -62,13 +62,12 @@ if (!is_null($events['events'])) {
 			sleep(10);
 			// Make a POST Request to Messaging API to Push to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
-			$userId = 'U97ccc6dbb284fd98f20f0ae3b25631c1';
 			$surveyQuestion = "ต้องการสอบถามข้อมูลเพิ่มเติมไหมครับ";
 			$abc = $json['userid'];
 			$messages = [
 				'type' => 'text',
 				//'text' => 'Bot Response: '.$text.$json['message']
-				'text' => $abc
+				'text' => $surveyQuestion
 			];
 
 			$data = [
